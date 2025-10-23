ca15457ba80f5bf996cbdd735487bb6cc49a0965
Hassel-Godspower <godspowerh0@gmail.com>
2025-10-23 15:31:09 +0100

    Delete test directory

--- test/test.js
+++
@@ -1,6 +0,0 @@
-import { analyzeSkinColor } from "../index.js";
-
-console.log("Testing Skin Tone Detector...");
-const sample = { r: 205, g: 150, b: 107 };
-console.log(analyzeSkinColor(sample));
-
